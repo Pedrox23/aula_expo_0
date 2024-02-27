b8de9609008c4ae806f7703b8dd96ec6fc881d9a
Pedrox23 <160646948+Pedrox23@users.noreply.github.com>
2024-02-27 15:24:10 -0300

    Aula 27-02

---
+++ components/exemplo_0/index.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+
+function Exemplo_0() {
+ return(
+    <viem>
+        <text> Minha Primeira Tela</text>
+    </viem>
+ );
+
+}
+
+export default Exemplo_0;
