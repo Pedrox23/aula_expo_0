50b9f5b9888d74084397878ca5a1263e17e6bd1b
Pedrox23 <160646948+Pedrox23@users.noreply.github.com>
2024-03-12 15:24:01 -0300

    Aula 12-03

---
+++ components/atividade_1/index.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native';
+import styles from './styles.js';
+function Atividade1() {
+ return(
+    <View style={styles.container}>
+        <Text style={styles.texto}>Atividade 1</Text>
+        <Text style={styles.texto}>Pedro Martins</Text>
+    </View>
+ );
+
+}
+
+export default Atividade1;
---
+++ components/atividade_1/styles.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from 'react-native';
+
+const style= StyleSheet.create({
+    container: {
+        backgroundColor: '#0f0',
+        padding:10,
+
+    },
+    texto: {
+        color: '#00f',
+        fontSize: 20,
+    }
+    
+    
+   
+});
+
+export default style;
