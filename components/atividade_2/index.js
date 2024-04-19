a7b5bf4b5d972a334ae4f10c9a767080c6493989
Pedrox23 <160646948+Pedrox23@users.noreply.github.com>
2024-04-19 09:48:44 -0300

    Aula 19-04

---
+++ components/atividade_2/index.js
@@ -0,0 +1,14 @@
+import { View, Text } from "react-native";
+import styles from ".atividade_2/styles.js";
+function Atividade_2() {
+ return(
+    <View style={styles.container}>
+        <Text style={styles.titulo}>
+            Atividade 2
+        </Text>
+        
+    </View>
+ );
+
+}
+
