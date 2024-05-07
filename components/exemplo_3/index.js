f8f403766a70311c0d880913621fb0dd1c830348
Pedrox23 <160646948+Pedrox23@users.noreply.github.com>
2024-05-07 15:22:51 -0300

    aula 07-05

--- components/exemplo_3/index.js
+++ components/exemplo_3/index.js
@@ -1,14 +1,20 @@
-import {View, Text, Pressable} from 'react-native';
+import { View, Text, Pressable } from 'react-native';
 
 import styles from './styles';
 
- function exemplo_3 () {
-    return(
+function exemplo_3() {
+    return (
         <View style={styles.container}>
             <Text style={styles.titulo}> Exemplo 3</Text>
-        <Pressable onPress={() => alert('Oi')} style={styles.botao}>
-        <Text>Ex. botão</Text>
-        </Pressable>
+            <Pressable
+                onPress={() => alert('Oi')}
+                style={({pressed}) => pressed ? 
+                [styles.botao, styles.botaoPress]
+                : 
+                styles.botao}
+            >
+                <Text style={styles.txtBotao}>Ex. botão</Text>
+            </Pressable>
 
         </View>
 
--- components/exemplo_3/styles.js
+++ components/exemplo_3/styles.js
@@ -20,7 +20,15 @@ const styles = StyleSheet.create({
         width: '80%',
         borderRadius: 20,
         alignItems: 'center',
-    }
+        borderColor: '#427ad4',
+    },
+    txtBotao:{
+        fontSize: 25,
+        color: '#673AB7',
+    },
+    botaoPress:{
+        backgroundColor: 'white',
+    },
 
 });
 
